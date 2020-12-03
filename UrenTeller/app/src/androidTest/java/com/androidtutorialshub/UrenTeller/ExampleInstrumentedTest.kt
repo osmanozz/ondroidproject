d963e448d2f8ecd52050fda6c0c74ea047ee731e
osmanozz <2120601@talnet.nl>
2020-12-03 15:25:05 +0100

    First commit Osman

---
+++ UrenTeller/app/src/androidTest/java/com/androidtutorialshub/UrenTeller/ExampleInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.androidtutorialshub.UrenTeller
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.androidtutorialshub.loginregisterkotlin", appContext.packageName)
+    }
+}
